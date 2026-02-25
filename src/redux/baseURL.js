b1c2e94ce3a4149475c664cefe0ea53375ee6bf3
Abdurrahman-Alizada <63086457+Abdurrahman-Alizada@users.noreply.github.com>
2026-02-25 06:19:39 +0500

    ad

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,2 +1,2 @@
 // export const baseURL = 'https://lanza-backend.onrender.com'
-export const baseURL = 'https://a1ca-223-123-88-255.ngrok-free.app'
+export const baseURL = 'https://2ef7-2407-d000-8-6c3a-848-3d09-f38d-d7e5.ngrok-free.app'
